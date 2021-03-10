46f9aa4146be29cf8aa480c5a6cf87ba770e606b
Andrei Silva <67024788+Andrei1234853@users.noreply.github.com>
2021-03-10 17:13:26 -0300

    Add files via upload

---
+++ atividade 4 de app/Biblioteca.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Biblioteca {
+
+public class Main {
+    
+        public static void main (String [] args){
+        Scanner ler = new Scanner(System.in);
+        
+        Livro l1 = new Livro();
+        Cliente c1 = new Cliente();
+        
+        System.out.println("Vamos fazer o cadastro! ");
+        System.out.println("Digita seu nome: ");
+        c1.setNome(ler.nextLine());
+        System.out.println("Digite sua idade: ");
+        c1.setIdade(ler.nextInt());
+        
+        System.out.println(c1);
+        
+        System.out.println("Agora está na hora de ver que livro tu quer manin ! ");
+        System.out.println("Digite o nome do livro: ");
+        l1.setNome(ler.nextLine());
+        System.out.println("Digite o genero do livro que você deseja: ");
+        l1.setGenero(ler.nextLine());
+        System.out.println("Digite o valor do livro: ");
+        l1.setValor(ler.nextDouble());
+        
+        System.out.println(l1);
+        System.out.println(l1.vereficarDesconto());
+        System.out.println(c1.verificarIdade());
+                
+    }
+    
+}
+    
+}
